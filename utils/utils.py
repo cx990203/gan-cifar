7c2326abdc594b54195736d808c8e668c725adf2
Mr.Chen <77096562+cx990203@users.noreply.github.com>
2022-06-12 17:09:38 +0800

    Add files via upload

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,32 @@
+import torch
+from torch import nn
+from torch.nn import init
 
+
+def weight_init(m):
+    if type(m) in [nn.ConvTranspose2d, nn.Conv2d]:
+        init.xavier_normal_(m.weight)
+    elif type(m) == nn.BatchNorm2d:
+        init.normal_(m.weight, 1.0, 0.02)
+        init.constant_(m.bias, 0)
+
+
+def weights_init(net, init_type='normal', init_gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and classname.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                torch.nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
